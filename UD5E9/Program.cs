e24199577343b72ba53de10f3496a20ac9d7e9a2
Martina <martinaescorial9@gmail.com>
2021-06-24 17:32:31 +0200

    Add project files.

---
+++ UD5E9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UD5E9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String numero1 = "Introduce un numero";
+            int elemento1 = Convert.ToInt32(numero1);
+
+            String numero2 = "Introduce un numero";
+            int elemento2 = Convert.ToInt32(numero2);
+
+            String numero3 = "Introduce un numero";
+            int elemento3 = Convert.ToInt32(numero3);
+
+            int[] array = { elemento1, elemento2, elemento3 };
+
+            Console.WriteLine(array);
+        }
+    }
+}
